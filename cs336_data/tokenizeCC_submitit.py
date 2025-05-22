eda644de5efa98b59839c2cbe5e8a492bca25910
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-22 12:01:45 -0700

    tokenize CC

---
+++ cs336_data/tokenizeCC_submitit.py
@@ -0,0 +1,60 @@
+import os 
+import submitit
+import pathlib
+from tqdm import tqdm
+
+from cs336_data.tokenize_cluster import tokenize_single_file
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', type=str, default="/data/c-salzhu/tokenizedCC_strict_0522/")
+parser.add_argument('--CC_filtered', type=str, default='/data/c-salzhu/filteredCC_strict_0522/')
+args = parser.parse_args()
+
+CC_filtered_path = args.CC
+output_directory_path = args.output 
+
+if not os.path.exists(output_directory_path):
+    os.makedirs(output_directory_path)
+
+# Set up the submitit executor
+executor = submitit.AutoExecutor(folder="slurm_logs")
+max_simultaneous_jobs = 16
+filteredCC_filepaths = []
+for root, _, files in os.walk(CC_filtered_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        filteredCC_filepaths.append(file_path)
+print(len(filteredCC_filepaths))
+
+# Configure parameters of each job launched by submitit
+executor.update_parameters(
+    slurm_array_parallelism=max_simultaneous_jobs,
+    timeout_min=15,
+    mem_gb=2,
+    cpus_per_task=1,
+    slurm_account="student",
+    slurm_partition="a4-cpu",
+    slurm_qos="a4-cpu-qos",
+)
+futures = []
+# Use exector.batch() context manager to group all of the jobs in a Slurm array
+with executor.batch():
+    for filteredCC_filepath in filteredCC_filepaths:
+        # For each WARC filepath, submit a job to the executor and get a future back
+        wet_filename = str(pathlib.Path(filteredCC_filepath).name)
+        future = executor.submit(
+            tokenize_single_file,
+            filteredCC_filepath,
+            output_directory_path
+        )
+        # Store the futures
+        futures.append(future)
+
+# Use tqdm to display progress
+for future in tqdm(
+    submitit.helpers.as_completed(futures),
+    total=len(filteredCC_filepaths),
+    ):
+    output_file = future.result()
+    print(f"Output file written: {output_file}")
---
+++ cs336_data/tokenize_test.py
@@ -0,0 +1,44 @@
+import multiprocessing
+
+import numpy as np
+from tqdm import tqdm
+from transformers import AutoTokenizer
+
+input_path = '/Users/sallyzhu/Desktop/cs336/assignment4-data/cs336_data/data/CC/filtered/CC-MAIN-20250430220529-20250501010529-00961.txt'
+output_path = '/Users/sallyzhu/Desktop/cs336/assignment4-data/cs336_data/data/CC/tokenized/test.bin'
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+def tokenize_line_and_add_eos(line):
+    return tokenizer.encode(line) #+ [tokenizer.eos_token_id] # there already is <|endoftext|> token 
+
+if __name__ == '__main__':
+
+    with open(input_path) as f:
+        lines = f.readlines()
+
+    # pool = multiprocessing.Pool(1) # multiprocessing.cpu_count()
+    # chunksize = 100
+    # results = []
+    # for result in tqdm(
+    #     pool.imap(tokenize_line_and_add_eos, lines, chunksize=chunksize),
+    #     total=len(lines),
+    #     desc="Tokenizing lines"
+    # ):
+    #     results.append(result)
+
+    # pool.close()
+    # pool.join()
+
+    results = []
+
+    for line in lines: 
+        results.append(tokenize_line_and_add_eos(line))
+
+    # Flatten the list of ids and convert to numpy array
+    all_ids = [token_id for sublist in results for token_id in sublist]
+    print(f"Tokenized and encoded {input_path} into {len(all_ids)} tokens")
+    ids_array = np.array(all_ids, dtype=np.uint16)
+    ids_array.tofile(output_path)
+
+    print(all_ids[-30:])
