ff2cdec2cbba900e99079521b96e92a7a7217e97
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-22 17:29:40 -0700

    process tokenized CC data

--- cs336_data/merge_tokenized.py
+++ cs336_data/merge_tokenized.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 directory_path = '/data/c-salzhu/tokenizedCC_strict_0522_5/'
 
 # Specify the output file name
-output_file = '/data/c-salzhu/CC_tokenized_full_0522_5.bin'
+output_file = '/data/c-salzhu/CC_tokenized_full_0522_5'
 
 # List to store all the arrays
 all_arrays = []
@@ -29,10 +29,10 @@ for filename in tqdm(os.listdir(directory_path)):
 # combined_array = np.concatenate(all_arrays)
 
 # Save the combined array to a new .bin file
-output_path = os.path.join(directory_path, output_file)
 combined_array = np.array(all_arrays, dtype=np.uint16)
-combined_array.tofile(output_path)
+combined_array.tofile(f'{output_file}.bin')
+np.save(f'{output_file}.npy', combined_array)
 
-print(f"Combined array saved to: {output_path}")
+print(f"Combined array saved to: {output_file}")
 print(f'Num tokens: {len(combined_array)}')
 print(f"Shape of all array: {all_arrays.shape}, {combined_array.shape}")
